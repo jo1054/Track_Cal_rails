d252794b74630752ca4661276c4d79e29350faea
Jonathan <jonathan.ojeda01@utrgv.edu>
2017-12-09 18:53:14 -0800

    navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,8 @@ class ApplicationController < ActionController::Base
 	def new
 
 	end
+	def comp
+
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 root :to => 'application#home'
 
 get 'home', to: 'application#home'
+get 'comp', to: 'application#comp'
 #devise_for :users, controllers: { sessions: 'users/sessions' }
 get 'new', to: 'application#new'
 #devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
